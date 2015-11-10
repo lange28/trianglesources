013ddc2e18e4741b3114bce4005311834e395d22
lange28 <lange28@mail.ru>
2015-11-10 14:35:57 +0300

    one simple test included

---
+++ UnitTests/UnitTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using triangle;
+
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            double a = 12, b = 4, c = 5;
+            double s = RightTr.Area(a,b,c);
+        }
+    }
+}
