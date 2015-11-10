15f4a2ad453e1931087a7a123afa31efe15344e0
lange28 <lange28@mail.ru>
2015-11-10 14:05:31 +0300

    func almost done

--- triangle/triangle/FunTr.cs
+++ triangle/triangle/FunTr.cs
@@ -6,15 +6,37 @@ using System.Threading.Tasks;
 
 namespace triangle
 {
-   public static class FunTr
+   public static class RightTr
     {
-        public static double Sq(double a, double b, double c)
+        public static double Area(double a, double b, double c)
         {
-            double res = 0;
+            //ArgumentException("One of the arguments are greater then the sum of others")
+            double value = 0;
             if ((a <= 0) || (b <= 0) || (c <= 0)) { throw new ArgumentOutOfRangeException("ds"); }
-            if ((a + b >= c) || (a + c >= b) || (c + b >= a)) { throw new ArgumentException("One of the arguments are greater then the sum of others"); }
-            res = 0.5 * a * b;
-            return res;
+            if ((a + b <= c) || (a + c <= b) || (c + b <= a)) { throw new ArgumentException("One of the arguments are greater then the sum of others"); }
+            value = 0.5 * a * b;
+            return value;
         }
+        public static void Main()
+        {
+            string str;
+            Console.WriteLine("Input a");
+            str = Console.ReadLine();
+            double a = Convert.ToDouble(str);
+
+            Console.WriteLine("Input b");
+            str = Console.ReadLine();
+            double b = Convert.ToDouble(str);
+
+            Console.WriteLine("Input c");
+            str = Console.ReadLine();
+            double c = Convert.ToDouble(str);
+            
+            double s = Area(a, b, c);
+
+            Console.WriteLine("Result "+Convert.ToString(s));
+            str = Console.ReadLine();
+        }
+
     }
 }
